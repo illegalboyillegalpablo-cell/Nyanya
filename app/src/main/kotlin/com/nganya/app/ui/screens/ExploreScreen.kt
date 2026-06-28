7d0ad2a3151640c6c580a1ff443fd09b69095535
illegalboyillegalpablo-cell <illegalboyillegalpablo@gmail.com>
2026-06-28 16:43:57 +0300

    Add Explore Screen UI

---
+++ app/src/main/kotlin/com/nganya/app/ui/screens/ExploreScreen.kt
@@ -0,0 +1,23 @@
+package com.nganya.app.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ExploreScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Explore Screen",
+            fontSize = 28.sp
+        )
+    }
+}
