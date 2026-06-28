2182ba33dd30ec8046825da4dbbc1b70640c042e
illegalboyillegalpablo-cell <illegalboyillegalpablo@gmail.com>
2026-06-28 16:42:58 +0300

    Update MainActivity to use NGANYAApp navigation

--- app/src/main/kotlin/com/nganya/app/MainActivity.kt
+++ app/src/main/kotlin/com/nganya/app/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nganya.app.ui.theme.NGANYATheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +14,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NGANYATheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("NGANYA")
+                    NGANYAApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Welcome to $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NGANYATheme {
-        Greeting("NGANYA")
     }
 }
